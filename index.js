9ff244e6f197cf2a1ee482af39b0ea2c3191dfec
SirYurko <55402601+SirYurko@users.noreply.github.com>
2024-12-22 16:17:40 +0200

    update form

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+// Функція для перевірки валідності email
+function validateEmail(email) {
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return emailPattern.test(email);
+}
 
+document.getElementById('myform').addEventListener('submit', checkForm);
 
-function checkForm(event, el){
+function checkForm(event){
     event.preventDefault();
+    let el = document.getElementById('myform');
 
     let username = el.username.value;
     let email = el.email.value;
@@ -22,25 +29,26 @@ function checkForm(event, el){
         error = "Ім'я занадто мале";
     }else if(email.length <5){
         error = "Email занадто малий";
-    }else if (!email.includes("@")){
+    }else if (!validateEmail(email)){
         error = "Email введено не коректно";
     }else if(password.length <6){
         error = "Пароль надто малий";
-    }else if(gender == null || gender == ''){
-        error = "gender error";
+    }else if(!gender){
+        error = "Виберіть стать";
     }
 
     // Виведення помилки або успіху
     let errorMessage = document.getElementById('errorMessage');
+    
     if (error !== '') {
         errorMessage.innerText = error;
+        errorMessage.classList.add('error-message');
         return false;
     }
 
 
     errorMessage.innerText = "Форма відправлена успішно";
-    errorMessage.style.color = 'green';
-    errorMessage.style.backgroundColor = 'white'
+    errorMessage.classList.add('success-message'); 
 
     window.location = 'https://github.com/SirYurko' // після вірної форми перекидує на сторінку
     console.log(userDate)
