1852a337b742c9036a64dc860ffd1cead2c9651a
SirYurko <55402601+SirYurko@users.noreply.github.com>
2024-12-22 15:32:28 +0200

    update singIn form

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
-let block = document.getElementById('some-block');
 
-block.innerHTML = 'Цей текст був записаний за допомогою, <b>innerHTML в JS коді</b>';
-block.style.color = 'red';
-block.style.backgroundColor = '#333';
-block.className = 'block';
 
-console.log("Ми додали клас для змінної " + "Class: " + block.className)
+function checkForm(event, el){
+    event.preventDefault();
 
-document.getElementById('para').innerHTML = "Some Text by getElementById"
-
-// let spans = document.getElementsByTagName('span');
-let spans = document.getElementsByClassName("el_span");
-
-for(let i = 0; i < spans.length; i++){  // проходимось по кожному елементу в масиві
-    spans[i].style.fontSize = "25px"    // для кожного елементу збільшуємо шрифт
-    spans[i].title = spans[i].innerText + "!"; // для кожного елемента ми додали спец текст при наведенні
-}
-
-let paragraphs = document.getElementsByTagName('p')
-
-for(let i = 0; i < paragraphs.length; i++){
-    paragraphs[i].style.color = "red" // Змінюємо стиль кожного параграфа
-}
-
-paragraphs[0].style.color = 'green' // Звертаємось до першого елемента
-
-paragraphs[1].style.fontSize = "23px"
-
-let form = document.getElementsByTagName('form')
-
-function checkForm(el){
     let username = el.username.value;
     let email = el.email.value;
     let gender = el.gender.value;
-
+    let password = el.password.value;
+    
+    //створюємо об'єкт для заведення користувача в базу
     let userDate = {
-        NickName: username,
+        userName: username,
         userEmail: email,
-        gender: gender,
+        userGender: gender,
+        userPassword: password,
+    }
+    let error = '';
+
+    //перевірка
+    if(username.length < 2){
+        error = "Ім'я занадто мале";
+    }else if(email.length <5){
+        error = "Email занадто малий";
+    }else if (!email.includes("@")){
+        error = "Email введено не коректно";
+    }else if(password.length <6){
+        error = "Пароль надто малий";
+    }else if(gender == null || gender == ''){
+        error = "gender error";
     }
 
-    // if(document.getElementById("male").checked){
-    //     userDate.gender = "Male";
-    // }else if (document.getElementById("female").checked){
-    //     userDate.gender = "Female";
-    // }else console.log("Gender undefinded")
+    // Виведення помилки або успіху
+    let errorMessage = document.getElementById('errorMessage');
+    if (error !== '') {
+        errorMessage.innerText = error;
+        return false;
+    }
 
+
+    errorMessage.innerText = "Форма відправлена успішно";
+    errorMessage.style.color = 'green';
+    errorMessage.style.backgroundColor = 'white'
+
+    window.location = 'https://github.com/SirYurko' // після вірної форми перекидує на сторінку
     console.log(userDate)
-    return false
+    
+    
+
+    return false; // Повертаємо false, щоб форма не відправлялась
 }
 
